04987c31ef40e596e7a76bf4838db9ec6115e591
Sean <51032338+Seanm64@users.noreply.github.com>
2023-02-12 14:58:20 -0500

    Add files via upload
    Updated

--- DAA/src/Programming_Project_Satisfiability_Checker/Clause.java
+++ DAA/src/Programming_Project_Satisfiability_Checker/Clause.java
@@ -5,14 +5,40 @@ import java.util.Map;
 
 public class Clause {
     private boolean result = false;
-    Map<Integer, Variable> normalMap = new HashMap();
-    Map<Integer, Variable> inverseMap = new HashMap();
+    Map<Integer, Boolean> map = new HashMap(); //Hashmap of integer and boolean (True = NOT, false = normal)
 
-    public void createClause(String[] load)
+
+    public Clause(String[] load) {
+        createClause(load);
+    }
+
+    public Clause() {
+    }
+    public void createClause(String[] load) {
+        //This is the raw data, no comments should get through
+
+        //Any positive number that comes through, set it's Boolean to FALSE
+        for (int i = 0; i < load.length; i++) {
+            int put = Integer.parseInt(load[i]);
+
+            if (put > 0)
+                map.put(put, false);
+            if (put < 0)
+                map.put(Math.abs(put), true);
+        }
+    }
+
+    public void addVariable(int put)
     {
-
+        if (put > 0)
+            map.put(put, false);
+        if (put < 0)
+            map.put(Math.abs(put), true);
     }
 
 
-
+    public boolean getBool(int i)
+    {
+        return map.get(i);
+    }
 }
--- DAA/src/Programming_Project_Satisfiability_Checker/Driver.java
+++ DAA/src/Programming_Project_Satisfiability_Checker/Driver.java
@@ -2,21 +2,40 @@ package Programming_Project_Satisfiability_Checker;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
+        List<Clause> clauseList = new LinkedList<>(); // The overarching structure is a LinkedList of clauses, if one clause is false
+                                                          //then it's not a viable solution
+
+
         //Take in a .txt file, (Figure out how to figure out the name of the files)
         File dir = new File("fileFolder");
         File[] files = dir.listFiles();
-
         Scanner fileReader = new Scanner(files[0]);   //READING IN DATA FROM fileFolder
 
+        //Need a for loop to loop through the folder of files!
 
-        //Get to the header (p15) Ignore the comments
-        String[] input = fileReader.nextLine().split(" "); //Split the whole line by
+        String[] input;
+        while(fileReader.hasNext())
+        {
+            Clause clause = new Clause();
+            input = fileReader.nextLine().split(" "); //Split the whole line by " "
+            if(input[0] != "c" || input[0] != "p")
+            {
+                for(int i = 0; i < input.length; i++)
+                {
+                    if(input[i] == "0")
+                        break;
 
-        System.out.println(input[0]);
+                    clause.addVariable(Integer.parseInt(input[i]));
+                }
+            }
+            input = fileReader.nextLine().split(" "); //Move onto the next line
+        }
 
         //with the p15 or s15, create an array of the number of variables of booleans (I think)
 
