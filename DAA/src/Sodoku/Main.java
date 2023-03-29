1af8956b863894ba66c713d40c22d9f57be86382
Sean <51032338+Seanm64@users.noreply.github.com>
2023-03-29 16:03:16 -0400

    Sat4j up and working

--- DAA/src/Sodoku/Main.java
+++ DAA/src/Sodoku/Main.java
@@ -1,7 +1,12 @@
-package Sodoku;
+package org.example;
 /** Sodoku - Transform and Conquer algorithm
  * @author Sean McKay
  */
+
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.ISolver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,38 +15,69 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, ContradictionException {
         File dir = new File("sodokuInputs");
         File[] files = dir.listFiles();
+
         Scanner fileReader = new Scanner(files[0]);   //READING IN DATA FROM sodokuInputs
         ISolver solver = SolverFactory.newDefault();
+
 
         int boxLength, boxWidth;
         int rowCounter = 0;
         int[] clause = {0, 0, 0};
-        List clauseList = new ArrayList<>(); //May not need this, can just submit everything into the Sat4j I think
+        List clauseList = new ArrayList<>();
         String[] input;
 
+        //Finding and creating the sodokuContainer
         boxLength = Integer.parseInt(fileReader.nextLine());
+        boxLength = boxLength * boxLength;
         boxWidth = Integer.parseInt(fileReader.nextLine());
+        boxWidth = boxWidth * boxWidth;
+        int[][] sodokuContainer = new int[boxLength][boxWidth];
 
         while (fileReader.hasNext()) {
             input = fileReader.nextLine().split(" "); //Split the whole line by " "
 
             rowCounter++;
-            for(int columnCounter = 0; columnCounter < input.length; columnCounter++)
+            for(int columnCounter = 1; columnCounter < input.length+1; columnCounter++)
             {
                 clause[0] = rowCounter;
                 clause[1] = columnCounter;
-                clause[2] = Integer.parseInt(input[columnCounter]);
+                clause[2] = Integer.parseInt(input[columnCounter-1]);
+                sodokuContainer[rowCounter-1][columnCounter-1] = clause[2];
+
 
                 if(clause[2] != 0)
                 {
-//                    clauseList.add(clause);
-                    System.out.println(clause[0] + " " + clause[1] + " " + clause[2]) ;
-//                    System.out.println(clauseList.get(columnCounter));
+                    clauseList.add(clause);
+//                    solver.addClause(new VecInt((Integer) clauseList.get(0)));
+//                    System.out.println(clause[0] + " " + clause[1] + " " + clause[2]) ;
                 }
             }
+        }
+
+        //Generate Every Clause Function
+
+
+        //Add all the clauses to the solver
+
+
+        //See if the solver can do it's thang
+
+
+        //Reverse engineer it
+
+
+        //Print out new 2D array (if can)
+
+
+        //Trying to print out the whole sodoku List to see what's in it
+        for(int i = 0; i < boxLength; i++) {
+            for (int j = 0; j < boxWidth ; j++) {
+                System.out.print(sodokuContainer[i][j] + " ");
+            }
+            System.out.println("");
         }
     }
 
